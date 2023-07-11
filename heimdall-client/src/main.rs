5d484648fe0862d88cd95c73ccb77a7a7f80e3c3
John Nystrom <eta077@yahoo.com>
2023-07-10 21:46:24 -0500

    started extracting hardcoded config values

--- heimdall-client/src/main.rs
+++ heimdall-client/src/main.rs
@@ -5,11 +5,15 @@ use sysinfo::{CpuExt, System, SystemExt};
 use tracing::error;
 
 use std::io::prelude::*;
-use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4, TcpStream};
+use std::net::{SocketAddr, TcpStream};
 use std::thread;
 use std::time::Duration;
 
-const ADDRESS: SocketAddr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), 4064));
+struct HeimdallClientConfig {
+    server_address: SocketAddr,
+    device_name: String,
+}
+
 const DELAY_MS: u64 = 1500;
 
 fn main() {
@@ -18,15 +22,32 @@ fn main() {
     let mut stream = None;
     let mut sys = System::new_all();
 
+    let server_address;
+    match "127.0.0.1:4064".parse() {
+        Ok(address) => {
+            server_address = address;
+        }
+        Err(e) => {
+            error!("Heimdall client unable to parse server address: {e}");
+            return;
+        }
+    }
+
+    let config = HeimdallClientConfig {
+        server_address,
+        device_name: String::from("Laptop"),
+    };
+
     loop {
         sys.refresh_all();
         let cpu_usage =
             sys.cpus().iter().map(|cpu| cpu.cpu_usage()).sum::<f32>() / sys.cpus().len() as f32;
         let mem_usage = (sys.used_memory() as f64 / sys.total_memory() as f64) as f32 * 100.0;
         send_update(
+            &config,
             &mut stream,
             DeviceUpdateMessage::CpuMem {
-                name: String::from("Laptop"),
+                name: config.device_name.clone(),
                 cpu: cpu_usage,
                 mem: mem_usage,
             },
@@ -35,9 +56,14 @@ fn main() {
     }
 }
 
-fn send_update(stream_opt: &mut Option<TcpStream>, msg: DeviceUpdateMessage) {
+fn send_update(
+    config: &HeimdallClientConfig,
+    stream_opt: &mut Option<TcpStream>,
+    msg: DeviceUpdateMessage,
+) {
     if stream_opt.is_none() {
-        let stream_result = TcpStream::connect_timeout(&ADDRESS, Duration::from_millis(DELAY_MS));
+        let stream_result =
+            TcpStream::connect_timeout(&config.server_address, Duration::from_millis(DELAY_MS));
         match stream_result {
             Ok(stream) => {
                 *stream_opt = Some(stream);
